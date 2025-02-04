dda0ca8843f7f69627a5253bf3f73c212c163cad
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-02-04 15:03:44 +0100

    feat: implement AI sesssion

---
+++ features/ai/di.ts
@@ -0,0 +1,18 @@
+import { container } from "tsyringe";
+import AiRoute from "./route";
+import AiController from "./controller";
+import SessionHandler from "./session/handler";
+
+export const registerAiDi = () => {
+  container.register(SessionHandler.name, {
+    useClass: SessionHandler,
+  });
+
+  container.register(AiRoute.name, {
+    useClass: AiRoute,
+  });
+
+  container.register(AiController.name, {
+    useClass: AiController,
+  });
+};
---
+++ features/ai/route.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { inject, injectable } from "tsyringe";
+import AiController from "./controller";
+import { useAuth } from "./../../infrastructure/middlewares";
+
+@injectable()
+export default class AiRoute {
+  public readonly router: Router;
+  private readonly _aiController: AiController;
+
+  constructor(@inject(AiController.name) aiController: AiController) {
+    this.router = Router();
+    this._aiController = aiController;
+
+    this.registerRoutes();
+  }
+
+  public registerRoutes() {
+    this.router.post("/session", [useAuth], (req, res) =>
+      this._aiController.createSession(req, res)
+    );
+  }
+}
