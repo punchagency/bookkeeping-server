0284f21f6edb730fea7ffebd52f7b56170e987b4
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-02-06 18:00:06 +0100

    chore: setup sendgrid DI, organized DI imports based on categories

--- features/auth/signup/handler.ts
+++ features/auth/signup/handler.ts
@@ -1,13 +1,26 @@
 import { Request, Response } from "express";
-import { injectable } from "tsyringe";
+import { injectable, inject } from "tsyringe";
 
 import signupSchema from "./signup.dto";
 import { Result } from "./../../../application/result";
+import SendgridService from "./../../../infrastructure/config/packages/sendgrid";
 
 @injectable()
 export default class SignupHandler {
+  private readonly _sendgridService: SendgridService;
+
+  constructor(@inject(SendgridService.name) sendgridService: SendgridService) {
+    this._sendgridService = sendgridService;
+  }
+
   public async handle(req: Request, res: Response) {
     const values = await signupSchema.validateAsync(req.body);
+
+    await this._sendgridService.sendEmail(
+      "adedoyinemmanuel@punch.agency",
+      "Action Required: Testing 1,2,3",
+      "Hello World"
+    );
 
     return Result.Ok(values);
   }
