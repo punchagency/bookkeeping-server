a2e2b5266585ac0c76b4d7f608ad0ae7aa878254
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2025-02-17 13:17:55 +0100

    feat: add time-range expenses analysis for transactions

--- features/bank/get-transactions/get-transaction.dto.ts
+++ features/bank/get-transactions/get-transaction.dto.ts
@@ -1,7 +1,13 @@
 import Joi from "joi";
 
 export const getTransactionsSchema = Joi.object({
-    perPage: Joi.number().optional(),
-    currentPage: Joi.number().optional()
-});
+  perPage: Joi.number().optional(),
+  currentPage: Joi.number().optional(),
 
+  /**
+   * This will allow us get total expenses for a given time range.
+   * It will be used to calculate the total expenses for the selected time range.
+   * I don't want to make it a seperate API call because expenses are part of the transactions.
+   */
+  days: Joi.string().optional().valid("30", "60", "90", "all"),
+});
